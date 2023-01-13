3976d40f4cfa4122da707fb72d461efe0339da6d
Stephane_tcha <stephanetcha@gmail.com>
2023-01-13 21:10:28 +0100

    typage

--- index.js
+++
@@ -1,5 +0,0 @@
-if("" == 0){
-    alert('je ne comprends rien, je prefere aller à la piscine')
-} else {
-    alert('c normal le nest pas egal a 0')
-}
---
+++ index.ts
@@ -0,0 +1,9 @@
+type point = {
+    x : number,
+    y : number
+}
+
+const drawpt = (pt: point) => {
+    console.log(pt.x)
+    console.log(pt.y)
+}
